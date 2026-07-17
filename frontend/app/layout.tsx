969f1a1b81970b632ebe72de404ad41dbe4276da
vrxcode <varuntej196@gmail.com>
2026-07-17 15:35:10 +0530

    commit 4

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className={`${roboto.className} flex min-h-screen flex-col`}>
-        <div className="flex min-h-0 flex-1 flex-col">{children}</div>
+        <div className="min-h-0 flex-1">{children}</div>
         <Footer />
       </body>
     </html>
